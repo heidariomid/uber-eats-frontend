94ac08e5f18092602b0d3008a848b016a274f356
omidheidari88 <omid_heidari88@yahoo.com>
2022-06-03 19:14:32 +0430

    add categories

--- src/pages/client/Restaurants.tsx
+++ src/pages/client/Restaurants.tsx
@@ -1,38 +1,58 @@
 import {useQuery} from '@apollo/client';
 import {useState} from 'react';
+import ErrorSpan from '../../components/custom/ErrorSpan';
 import Loading from '../../components/loading/Loading';
-import {RESTAURANTS} from '../../graphql/queries';
-import {RestaurantsQuery, RestaurantsQueryVariables} from '../../graphql/schemaTypes';
+import {CATEGORIES, RESTAURANTS} from '../../graphql/queries';
+import {CategoriesQuery, CategoriesQueryVariables, RestaurantsQuery, RestaurantsQueryVariables} from '../../graphql/schemaTypes';
+import Category from './Category';
 import Restaurant from './Restaurant';
+import RestaurantsSearch from './RestaurantsSearch';
 interface IRestaurant {
 	id: number;
 	name: string;
 	isPromoted: boolean;
 }
 const Restaurants = () => {
-	const [serverMessage, setServerMessage] = useState('');
-	const [restaurants, setRestaurants] = useState<IRestaurant[]>();
-
-	const onCompleted = (data: RestaurantsQuery) => {
+	const [errorMessage, setErrorMessage] = useState('');
+	const [restaurants, setRestaurants] = useState<IRestaurant[]>([]);
+	const [categories, setCategories] = useState<any>([]);
+	const onCompletedRestaurants = (data: RestaurantsQuery) => {
 		const {ok, message, restaurants} = data?.getRestaurants;
 		if (!ok && message) {
-			setServerMessage(message);
+			setErrorMessage(message);
 		}
 		if (ok && restaurants) {
 			setRestaurants(restaurants);
 		}
 	};
-	const {loading} = useQuery<RestaurantsQuery, RestaurantsQueryVariables>(RESTAURANTS, {onCompleted, variables: {data: {page: 2}}});
+	const onCompletedCategories = (data: CategoriesQuery) => {
+		const {ok, message, categories} = data?.getCategories;
+		if (!ok && message) {
+			setErrorMessage(message);
+		}
+		if (ok && categories) {
+			setCategories(categories);
+		}
+	};
+	const {loading, error} = useQuery<RestaurantsQuery, RestaurantsQueryVariables>(RESTAURANTS, {onCompleted: onCompletedRestaurants, variables: {data: {page: 2}}});
+	const {loading: categoriesLoading, error: categoriesError} = useQuery<CategoriesQuery, CategoriesQueryVariables>(CATEGORIES, {onCompleted: onCompletedCategories});
 
 	return (
-		<div>
-			{serverMessage && <span className='bg-red-600 text-white px-4 span'>{serverMessage}</span>}
-			<form className='w-full flex items-center justify-center font-bold text-lg bg-black text-center py-14 my-4'>
-				<input className='input rounded-md border-0 w-3/6' type='search' placeholder='Search Restaurants...' />
-			</form>
-			{loading && <Loading />}
-			{!loading && restaurants && restaurants.map((restaurant) => <Restaurant key={restaurant.id} restaurant={restaurant} />)}
-		</div>
+		<>
+			{error && <ErrorSpan message={errorMessage} />}
+			{categoriesError && <ErrorSpan message={errorMessage} />}
+			{errorMessage && <ErrorSpan message={errorMessage} />}
+			{categoriesLoading && <Loading />}
+			{loading ? (
+				<Loading />
+			) : (
+				<>
+					<RestaurantsSearch />
+					{restaurants && restaurants.map((restaurant) => <Restaurant key={restaurant.id} restaurant={restaurant} />)}
+					{categories && categories.map((category) => <Category key={category.id} category={category} />)}
+				</>
+			)}
+		</>
 	);
 };
 
