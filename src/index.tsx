7f18852a1bc28ad69887a46cd741e96a41f23eb5
omidheidari88 <omid_heidari88@yahoo.com>
2022-06-10 21:09:19 +0430

    preapre testing

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
-
+import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<Router>
 		<App />
 	</Router>,
 );
+
+reportWebVitals();
