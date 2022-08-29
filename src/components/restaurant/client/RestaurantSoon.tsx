3d00b50b3bacb3e4c3e276835a88c068bdcb24c0
omidheidari88 <omid_heidari88@yahoo.com>
2022-08-29 12:09:07 +0430

    bug fix

--- src/components/restaurant/client/RestaurantSoon.tsx
+++ src/components/restaurant/client/RestaurantSoon.tsx
@@ -14,7 +14,7 @@ const RestaurantSoon = ({restaurant}) => {
 				/>
 
 				<div className={`absolute text-center flex justify-center items-center  text-5xl font-semibold ${isDark ? 'bg-black' : 'bg-white'} bg-opacity-50   py-10 px-20 `}>
-					<div className='pl-14 py-2 '>
+					<div>
 						<h1 className='font-bold mt-2 mb-10 text-3xl md:text-7xl'>Coming Soon</h1>
 						<h2 className='font-bold mt-2 md:text-4xl text-2xl text-left mb-5'>{restaurant?.name}</h2>
 						<div className='md:text-2xl text-lg mt-4 text-left uppercase mb-5'>{restaurant?.category?.name}</div>
